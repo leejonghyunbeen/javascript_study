b5754e284c27d8e44888c40b274e6144cfef7747
leejonghyunbeen <clfl19@naver.com>
2024-02-07 11:06:32 +0900

    math와 배열

---
+++ math/data.js
@@ -0,0 +1,12 @@
+const data = [
+    {
+        name:'토마토',
+        price:1000,
+    },{
+        name:'바나나',
+        price:2000,
+    },{
+        name:'사과',
+        price:3000,
+    }
+]
---
+++ math/shopping.js
@@ -0,0 +1,15 @@
+const shopping_data = [
+    {
+        name:'원피스',
+        price:2000,
+        color:'black',
+    },{
+        name:'스커트',
+        price:5000,
+        color:'white',
+    },{
+        name:'블라우스',
+        price:3000,
+        color:'black',
+    },
+]
